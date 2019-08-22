35fc40a3d58aee055233d25ca676ead830690074
Miguel Mexicano Herrera <miguelmexicano18@gmail.com>
2019-08-22 18:21:07 -0500

    add QueryDSL

--- src/main/java/mx/com/nut/backendninja/repository/QueryDSLExampleRepo.java
+++ src/main/java/mx/com/nut/backendninja/repository/QueryDSLExampleRepo.java
@@ -1,5 +1,12 @@
 package mx.com.nut.backendninja.repository;
 
+import org.springframework.stereotype.Repository;
+
+import mx.com.nut.backendninja.entity.QCourse;
+
+@Repository("queryDSLExampleRepo")
 public class QueryDSLExampleRepo {
+	
+	private QCourse qCourse = QCourse.course;
 
 }
